139a6c48eb6ac236845b08d3de7a82843a0d633b
istiakshovon <Istiakshovon001@gmail.com>
2021-01-21 21:44:35 +0600

    permission added

--- app/src/main/java/com/example/pdfreader/MainActivity.java
+++ app/src/main/java/com/example/pdfreader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pdfreader;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,6 +15,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.File;
 
@@ -34,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         btnFile = findViewById(R.id.btn_file);
         txt = findViewById(R.id.txt);
 
+        permission();
 
         btnFile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +67,11 @@ public class MainActivity extends AppCompatActivity {
 
             // When an file is picked
             if (requestCode == 3 && resultCode == RESULT_OK && null != data) {
+                String path = data.getStringExtra(mediaPath);
+                Log.d("PATH",path+" ");
+                String data1 = data.toString();
+                Log.d("DATA1",data1);
+
                 Uri PathHolder = data.getData();
                 File file = new File(PathHolder.toString());
                 Log.v("###", "yo " + PathHolder);
@@ -73,8 +86,8 @@ public class MainActivity extends AppCompatActivity {
                             int n = reader.getNumberOfPages();
                             for (int i = 10; i <n ; i++) {
                                 parsedText   = parsedText+ PdfTextExtractor.getTextFromPage(reader, i).trim()+"\n";
-                                Log.d("for loop", String.valueOf(i));
-                                Log.d("PARSED_TEXT",parsedText);
+                                Log.d("for_loop", String.valueOf(i));
+                                Log.d("PARSED_TEXT",parsedText+" ");
                             }
                             builder.append(parsedText);
 
@@ -99,6 +112,36 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+
+    private void permission(){
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+                        Toast.makeText(MainActivity.this, "permission granted", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        Toast.makeText(MainActivity.this, "Permission rational should be shown", Toast.LENGTH_SHORT).show();
+                    }
+                }).check();
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+                        Toast.makeText(MainActivity.this, "permission granted", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        Toast.makeText(MainActivity.this, "Permission rational should be shown", Toast.LENGTH_SHORT).show();
+                    }
+                }).check();
     }
 
 }
